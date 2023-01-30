bf4bd7fa15c3951ec0c4aea8d25f35596609aca5
Ascordio <vladyslav.fedyna@oa.edu.ua>
2023-01-30 16:56:59 +0200

    UserUpdate

---
+++ TaxiSevice/TaxiService.Core/TaxiServiceDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaxiService.Core
+{
+    public class TaxiServiceDbContext : IdentityDbContext<User>
+    {
+        public TaxiServiceDbContext(DbContextOptions<TaxiServiceDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
